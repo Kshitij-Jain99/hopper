a88b05d03e3b01cf59ae64fc6f8462ff0eba2a53
Kshitij Jain <kshitijjain6074@gmail.com>
2026-04-10 04:37:14 +0530

    initialize project

--- hopper-sb/src/main/java/com/hopper/urlshortener/HopperSbApplication.java
+++ hopper-sb/src/main/java/com/hopper/urlshortener/HopperSbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HopperApplication {
+public class HopperSbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HopperApplication.class, args);
+		SpringApplication.run(HopperSbApplication.class, args);
 	}
 
 }
