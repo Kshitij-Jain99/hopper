719ec9f26f05f149f7a5f79c5ccb1212ee689010
Kshitij Jain <kshitijjain6074@gmail.com>
2026-05-21 07:12:51 +0530

    adds JWT implementation

---
+++ hopper-sb/src/main/java/com/hopper/urlshortener/security/jwt/JwtUtils.java
@@ -0,0 +1,71 @@
+package com.hopper.urlshortener.security.jwt;
+
+import com.hopper.urlshortener.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+
+    // Authorization -> Bearer <Token>
+    public String getJwtFromHeader(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public String generateToken(UserDetailsImpl userDetails){
+        String username = userDetails.getUsername();
+        String roles = userDetails.getAuthorities().stream()
+                .map(authority -> authority.getAuthority())
+                .collect(Collectors.joining(","));
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date().getTime() + jwtExpirationMs)))
+                .signWith(key())
+                .compact();
+    }
+
+    public String getUserNameFromJwtToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    private SecretKey key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public boolean validateToken(String authToken){
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJws(authToken);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
--- hopper-sb/src/main/java/com/hopper/urlshortener/service/UserDetailsServiceImpl.java
+++ hopper-sb/src/main/java/com/hopper/urlshortener/service/UserDetailsServiceImpl.java
@@ -1,4 +1,25 @@
-package service;
+package com.hopper.urlshortener.service;
 
-public class UserDetailsServiceImpl {
+import com.hopper.urlshortener.models.User;
+import com.hopper.urlshortener.repository.UserRepository;
+import com.hopper.urlshortener.service.UserDetailsImpl;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found with username" + username));
+        return UserDetailsImpl.build(user);
+    }
 }
